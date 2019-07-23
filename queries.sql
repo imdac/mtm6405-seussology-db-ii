2f7fccc2d96b7a52ab6dac4e7156f9279a0f8da9
Michael Eisenbraun <eisenbm@algonquincollege.edu>
2019-07-23 15:07:59 -0400

    Adds starter files

---
+++ queries.sql
@@ -0,0 +1,38 @@
+-- 1. Insert a new quote into the `quotes` table with the following data: quote = "The fools that I saw were none other than you", book_id = 9
+
+
+
+-- 2. Using a subquery, insert the quote, "Just stew! Pooh." for the book "Steak for Supper" into the `quotes` table.
+
+
+
+-- 3. Update the book year (`book_year`) of the book with the title "Tadd and Todd" to 1950 in the `books` table. 
+
+
+
+-- 4. Using subqueries, update all of the quotes in the `quotes` table for the book “The Big Brag” to the book id for the book “Yertle the Turtle”
+
+
+
+-- 5. Remove the book "The Big Brag" from the `books` table. 
+
+
+
+-- 6. Using an INNER JOIN, retrieve the first 5 book titles (`book_title`) and category name (`category_name`) when the results are sorted by the book title sort (`book_title_sort`) column.
+
+
+
+-- 7. Retrieve the average number of book pages (`book_pages`) from the `books` table.
+
+
+
+-- 8. Retrieve the newest book title (`book_title`) from the `books` table that is in the category of "Beginner Books".
+
+
+
+-- 9. Retrieve the category id (`category_id`) and number of books in each category from the `books` table, and sort the results by the number of books with the highest number first.
+
+
+
+-- 10. Using INNER JOINs, retrieve the second set of 5 quotes (`quotes`) and book titles (`book_title`) for all of the books in the “Beginner’s Books” category when the results are sorted by the book title (`book_title`)
+
